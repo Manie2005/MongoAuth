0289d36b6dce5b1f96ac171e99f55f2e33a0cf58
Your Name~Manie2005 <maniesamuel24@gmail.com>
2024-10-07 18:25:23 +0100

    Update

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -2,4 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Axios } from 'axios';
  
 @Injectable()
-export class PaymentService {}
+export class PaymentService {
+    private readonly PAYSTACK_API_URL='https://api.paystack.co';
+    private readonly PAYSTACK_SECRET_KEY=process.env.PAYSTACK_SECRET_KEY;
+}
