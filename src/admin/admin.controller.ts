c23404522f9871d1594100f295f5ecc01f20a515
Your Name~Manie2005 <maniesamuel24@gmail.com>
2024-11-19 07:00:35 +0100

    Updated Commit

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -6,14 +6,14 @@ import { Error } from 'mongoose';
 @Controller('admin')
 export class AdminController {
     constructor(private readonly adminService: AdminService) {}
-//Admin Login
+//Admin Logins
 @Post('login')
   async login(@Body() loginDto: LoginDto) {
+
     const admin = await this.adminService.findByEmail(loginDto.email);
     if(!admin){
         throw new Error('Invalid Credentials');
-            }
-
+                    }
         }
 
 
